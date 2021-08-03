72483b7a386b364dc76d3be3550262d424b64d3b
Md. Samin Mustakim <78674084+saminmustakim@users.noreply.github.com>
2021-08-03 16:26:24 +0600

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from prime_number import prime_number
-
-if __name__ == "__main__":
-    n = int(input("Input a number: "))
-    prime = prime_number(n)
-    if prime is True:
-        print(n, "is a prime number")
-    else:
-        print(n, "is not a prime number")
